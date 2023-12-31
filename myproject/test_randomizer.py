91fe38a80ce3c4521b091303ae6f32adb88d17a9
Baisangur-Dudayev <r0930201@student.thomasmore.be>
2023-12-31 22:16:22 +0100

    d

--- myproject/test_randomizer.py
+++ myproject/test_randomizer.py
@@ -9,11 +9,11 @@ def test_get_books():
 
 def test_get_authors():
     response = requests.get('https://useritem-api-service-eindproject-baisangur-dudayev.cloud.okteto.net/authors/')
-    assert response.status_code == 401
+    assert response.status_code == 200
 
 def test_get_author():
     response = requests.get('https://useritem-api-service-eindproject-baisangur-dudayev.cloud.okteto.net/authors/1')
-    assert response.status_code == 401
+    assert response.status_code == 200
 
 
 
